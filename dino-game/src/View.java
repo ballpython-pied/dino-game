91554133bdf33c97d17ab03cce3c40c177c3cecf
Rian.Goldie <555harky116@gmail.com>
2024-05-13 13:08:33 -0700

    added the empty packages

--- dino-game/src/View.java
+++ dino-game/src/View.java
@@ -1,3 +1,33 @@
-public class View {
+import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.*;
+
+import java.util.*;
+import java.awt.*;
+import java.io.*;
+public class View extends JFrame{
+
+
+    private JButton playButton = new JButton("Play");
+
+    View() {
+
+        JPanel mainScreen = new JPanel();
+        JPanel gameScreen = new JPanel();
+
+
+        this.setSize(600,200);
+        mainScreen.setSize(600,200);
+        gameScreen.setSize(600,200);
+        this.add(mainScreen);
+        this.add(gameScreen);
+        mainScreen.add(playButton);
+        playButton.setSize(20,20);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+
     //this is the GUI class
 }
