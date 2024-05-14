4a59bfe9ae5e1f99620daff3b315d205bdf787fe
Rian.Goldie <555harky116@gmail.com>
2024-05-14 12:46:03 -0700

    rizz

--- dino-game/src/Control.java
+++ dino-game/src/Control.java
@@ -1,3 +1,20 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 public class Control {
     //this class contains the listeners that record user input (such as key presses)
+    private Main gameModel;
+    private View gameview;
+
+    public void gameController(Main gameModel, View gameview){
+        this.gameModel = gameModel;
+        this.gameview = gameview;
+
+        this.gameview.addPlayListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+    }
 }
--- dino-game/src/View.java
+++ dino-game/src/View.java
@@ -8,26 +8,30 @@ import java.util.*;
 import java.awt.*;
 import java.io.*;
 public class View extends JFrame{
-
-
     private JButton playButton = new JButton("Play");
-
-    View() {
-
+    public View() {
+        super();
         JPanel mainScreen = new JPanel();
         JPanel gameScreen = new JPanel();
 
 
-        this.setSize(600,200);
-        mainScreen.setSize(600,200);
-        gameScreen.setSize(600,200);
+        this.setLayout(null);
+        this.setSize(600,400);
+        mainScreen.setSize(600,400);
+        gameScreen.setSize(600,400);
         this.add(mainScreen);
         this.add(gameScreen);
         mainScreen.add(playButton);
         playButton.setSize(20,20);
+        this.setVisible(true);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
-
+    void addPlayListener(ActionListener playButtonListener){ //basically a setter/getter for the actionlistener.
+        playButton.addActionListener(playButtonListener);
+    }
+    public static void main(String[] args) {
+        new View();
+    }
 
     //this is the GUI class
 }
