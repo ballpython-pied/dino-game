bfd443553942a7a9f6c9883aa7a03b9130cd01f9
iniyazhhariharan <iniyazhh@gmail.com>
2024-05-16 12:25:42 -0700

    hi <3

--- dino-game/src/Player.java
+++ dino-game/src/Player.java
@@ -1,3 +1,10 @@
+import java.io.IOException;
+
 public class Player extends Obstacle{
+    public Player() throws IOException {
+    }
     //since a player technically has the characteristics of an obstacle, it can be inherited as an obstacle. we can add a few extra features for the player.
+
+
+
 }
