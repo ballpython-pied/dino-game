ae0cf5be1cb7606cb0975c8572fe5cf5cb316b34
Rian.Goldie <555harky116@gmail.com>
2024-05-24 12:53:55 -0700

    tried to get the function to work -- it works (kinda) but the image does not update

--- dino-game/src/Model.java
+++ dino-game/src/Model.java
@@ -11,32 +11,28 @@ int jumpcount = 28;
         //var is the variable that is scraped from the frame rate and ground is the constant variable that determines the ground height
             System.out.println("function ran");
 
-        Timer timer = new Timer(250, new ActionListener() {
-            //https://stackoverflow.com/questions/30419775/not-sure-how-to-put-timer-into-my-keylistener-code
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                while (jumpcount != 0) {
-                    int funcvar = (int) ((-1) * Math.pow((14 - jumpcount), 2) + (400));
-                    System.out.println(funcvar + " count: " + jumpcount);
-                    //\ -1\left(14-x\right)^{2}+200
-                    view.dino.setLocation(60, funcvar);
-                    view.repaint();
-                    view.revalidate();
-                    jumpcount--;
-
-
-                }
-
+        //https://stackoverflow.com/questions/30419775/not-sure-how-to-put-timer-into-my-keylistener-code
+        Timer timer = new Timer(50, e -> {
+            if (jumpcount != 0) {
+                int funcvar = (int) (Math.pow((14 - jumpcount), 2) + (150));
+                System.out.println(funcvar + " count: " + jumpcount);
+                //\ -1\left(14-x\right)^{2}+200
+                view.dino.setLocation(60, funcvar);
+                view.repaint();
+                view.revalidate();
+                jumpcount--;
             }
         });
 
         if (jumpcount == 0 && view.jump) {
-            timer.restart();
             jumpcount = 28;
             view.jump = false;
         }
+        else{
+            timer.start();
+        }
         timer.setInitialDelay(10);
-        timer.start();
+
 
 
 
